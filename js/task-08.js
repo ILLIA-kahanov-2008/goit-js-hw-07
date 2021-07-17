4254c1daf475c2d5579dbc23197c30830b197b5b
ILLIA Kahanov <kahanov.illia@gmail.com>
2021-07-17 19:15:43 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -18,27 +18,48 @@
 // <div id="boxes"></div>
 
 const refs = {
-  numberOfBoxes: document.querySelector('input[type=number]'),
+  numberOfBoxes: document.querySelector("input[type=number]"),
   renderBtn: document.querySelector('[data-action="render"]'),
   destroyBtn: document.querySelector('[data-action="destroy"]'),
-  controlsBlock: document.querySelector('#controls'),
-  boxesBlock: document.querySelector('#boxes'),
-}
+  controlsBlock: document.querySelector("#controls"),
+  boxesBlock: document.querySelector("#boxes"),
+};
 
-function createBoxes (event, amount) {
-  console.log(amount);
+const randomColor = () => {
+  const r = Math.round(Math.random() * (255 - 1) + 1);
+  const g = Math.round(Math.random() * (255 - 1) + 1);
+  const b = Math.round(Math.random() * (255 - 1) + 1);
+  return `rgb(${r},${g},${b})`;
+};
+
+function createBoxes(event, amount) {
+  refs.boxesBlock.innerHTML = "";
   if (amount > 0) {
-    // const boxSideSize = 30;
-    // let i = 1;
-    // while (i === amount) {
-      
-    // }
-    console.log(refs.numberOfBoxes.value);
-  } else { refs.boxesBlock.textContent = 'no boxes to create' }
+    let boxSideSize = 30;
+    for (let i = 1; i <= amount; i++) {
+      const boxColor = randomColor();
+      const box = document.createElement("div");
+      box.style.backgroundColor = boxColor;
+      box.style.width = `${boxSideSize}px`;
+      box.style.height = `${boxSideSize}px`;
+      box.style.borderColor = randomColor();
+      box.style.margin = "5px";
+      box.style.borderStyle = "solid";
+      box.style.borderWidth = "1px";
+      refs.boxesBlock.append(box);
+      boxSideSize += 10;
+    }
+  } else {
+    refs.boxesBlock.style.color = "red";
+    refs.boxesBlock.textContent = "no boxes to create, enter number > 0";
+  }
 }
 
-refs.numberOfBoxes.addListenerEvents('input', createBoxes(refs.numberOfBoxes.value))
+const destroyBoxes = () => refs.boxesBlock.innerHTML = "";
 
+refs.renderBtn.addEventListener("click", function (event) {
+  createBoxes(event, refs.numberOfBoxes.value);
+});
 
+refs.destroyBtn.addEventListener("click", destroyBoxes);
 
-console.log(refs.numberOfBoxes);
